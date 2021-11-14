b425cd3935cc79b6e127e0520d76df098cfebe47
vasvap <vasvap@gmail.com>
2021-11-14 23:21:32 +0300

    setDay added

--- src/test/java/ru/apolonov/pages/components/CalendarComponent.java
+++ src/test/java/ru/apolonov/pages/components/CalendarComponent.java
@@ -9,12 +9,16 @@ public class CalendarComponent {
             monthSelect = $(".react-datepicker__month-select"),
             yearSelect = $(".react-datepicker__year-select");
 
+    public void setDay(String day) {
+        SelenideElement daySelect =
+                $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)");
+        daySelect.click();
+    }
 
     public void setDate(String day, String month, String year) {
         dateInput.click();
         monthSelect.selectOption(month);
         yearSelect.selectOption(year);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
-
+        setDay("09");
     }
 }
