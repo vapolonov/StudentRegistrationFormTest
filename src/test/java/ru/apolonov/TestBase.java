f51aa1354dc85a415e6b8e3bb807f0f526373e10
vasvap <vasvap@gmail.com>
2021-11-24 20:35:25 +0300

    capabilities added

--- src/test/java/ru/apolonov/TestBase.java
+++ src/test/java/ru/apolonov/TestBase.java
@@ -1,7 +1,10 @@
 package ru.apolonov;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.apolonov.pages.RegistrationsPage;
 
 public class TestBase {
@@ -10,7 +13,15 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
 }
